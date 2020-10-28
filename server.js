9dbb2929104f77d0df164d41fbd86f4db2d58a98
oraculocs <augustino2003@hotmail.com>
2020-10-28 16:43:29 -0300

    Novo teste deploy heroku

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + 'dist/tejoao'));
+// app.use(express.static(__dirname));
 
 app.get('/*', (req, res) => {
+    // res.sendFile(__dirname + '/index.html')
     res.sendFile(__dirname + '/dist/tejoao/index.html')
 })
 
