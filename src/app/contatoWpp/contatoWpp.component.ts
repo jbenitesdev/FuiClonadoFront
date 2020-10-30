26237dc545c091949488b7f766af38bf6ad3a5b6
oraculocs <augustino2003@hotmail.com>
2020-10-30 18:45:37 -0300

    Inclusão da obtenção dos contatos da Google

--- src/app/contatoWpp/contatoWpp.component.ts
+++ src/app/contatoWpp/contatoWpp.component.ts
@@ -30,13 +30,16 @@ export class ContatoWppComponent implements OnInit {
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get('code');
 
-        if (code && code !== '' && code !== undefined) {
+        if (urlParams && code && code !== '' && code !== undefined) {
             console.log("EXECUTANDO REQUEST DO TOKEN")
             let googleClientId = "464781618950-f2b0bp5aubjdicbfj10kv7lqejv888vs.apps.googleusercontent.com";
             let googleClientSceret = "Y5I1R957rkEfpdviz8UiGcBC";
             let googleRedirectUrl = "https://fuiclonadoarq.herokuapp.com/contatosApi";
             
-            this.numerosClonadosService.getAccessToken(code, googleClientId, googleClientSceret, googleRedirectUrl).subscribe(res => {console.log("RES: ", res)})
+            this.numerosClonadosService.getAccessToken(code, googleClientId, googleClientSceret, googleRedirectUrl).subscribe(res => {
+                console.log("RES: ", res);
+                this.obterContatos(res.refresh_token, res.access_token)
+            })
         }
     }
 
@@ -45,7 +48,50 @@ export class ContatoWppComponent implements OnInit {
         const code = urlParams.get('code');
     }
 
+    obterContatos(refreshTokenResp: string, accessTokenResp: string) {
+        let refreshToken = refreshTokenResp;
+        let accessToken = accessTokenResp;
+        let scope = "https://www.google.com/m8/feeds/contacts/default/full/";
+
+        this.numerosClonadosService.getContatos(scope, accessToken, refreshToken).subscribe(respCon => { console.log("RESPONSE DOS CONTATOS: ", respCon)})
+    }
+
     ngOnInit() {
         this.getAccessToken()
     }
+
+    // public void GetContacts(refreshTokenResp: string, accessTokenResp: string, )
+    // {
+    //     /*Get Google Contacts From Access Token and Refresh Token*/
+    //     string refreshToken = serStatus.refresh_token;
+    //     string accessToken = serStatus.access_token;
+    //     string scopes = "https://www.google.com/m8/feeds/contacts/default/full/";
+    //     OAuth2Parameters oAuthparameters = new OAuth2Parameters()
+    //     {
+    //         Scope = scopes,
+    //         AccessToken = accessToken,
+    //         RefreshToken = refreshToken
+    //     };
+    
+    //     RequestSettings settings = new RequestSettings("<var>YOUR_APPLICATION_NAME</var>", oAuthparameters);
+    //     ContactsRequest cr = new ContactsRequest(settings);
+    //     ContactsQuery query = new ContactsQuery(ContactsQuery.CreateContactsUri("default"));
+    //     query.NumberToRetrieve = 5000;
+    //     Feed<Contact> feed = cr.Get<Contact>(query);
+        
+    //     StringBuilder sb = new StringBuilder();
+    //     int i = 1;
+    //     foreach (Contact entry in feed.Entries)
+    //     {
+    //         foreach (EMail email in entry.Emails)
+    //         {
+    //             sb.Append("<span>"+ i + ". </span>").Append(email.Address)
+    //               .Append("<br/>");
+    //             i++;
+    //         }
+    //     }
+    //     /*End*/
+    
+    //     dataDiv.InnerHtml = sb.ToString();
+    // }
 }
--- src/app/service/numeros-clonados.service.ts
+++ src/app/service/numeros-clonados.service.ts
@@ -37,4 +37,8 @@ export class NumerosClonadosService {
   public getAccessToken(code: string, googleClientId: string, googleClientSceret: string, googleRedirectUrl: string): Observable<any> {
     return this.http.post<Observable<any>>('https://accounts.google.com/o/oauth2/token', {code: code, client_id: googleClientId, client_secret: googleClientSceret, redirect_uri: googleRedirectUrl, grant_type: "authorization_code"})
   }
+
+  public getContatos(scope: string, accessToken: string, refreshToken: string): Observable<any> {
+    return this.http.get<Observable<any>>(`${scope}`);
+  }
 }
